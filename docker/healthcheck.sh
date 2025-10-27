0d0baa8dca7ce4eeedf95101fdba59f66b9ff61f
fm-labs <flowmotionlabs@gmail.com>
2025-10-27 10:23:07 +0100

    container healthcheck

--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-curl --silent --fail http://localhost:8000/
+#curl --silent --fail http://localhost:8000/
+curl --silent http://localhost:8000/
 if [ $? -ne 0 ]; then
   echo "Health check failed"
   exit 1
