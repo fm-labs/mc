016081df49c305e3550396638d610c769eb94ec1
fm-labs <flowmotionlabs@gmail.com>
2026-03-16 22:03:09 +0100

    minor

--- ui/src/components/layout/nav-user.tsx
+++ ui/src/components/layout/nav-user.tsx
@@ -56,7 +56,7 @@ export function NavUser() {
                         >
                             <Avatar className="h-8 w-8 rounded-lg grayscale">
                                 <AvatarImage src="/avatars/shadcn.jpg" alt={user.username} />
-                                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                                <AvatarFallback className="rounded-lg">MC</AvatarFallback>
                             </Avatar>
                             <div className="grid flex-1 text-left text-sm leading-tight">
                                 <span className="truncate font-medium">{user.username}</span>
