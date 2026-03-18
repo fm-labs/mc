2dc3409dac161d7c08ddc84e9bdca3446e9764de
fm-labs <flowmotionlabs@gmail.com>
2026-03-18 07:32:29 +0100

    ssl port

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@
 # - DOCKER_SOCKET: The path to the Docker socket (default: /var/run/docker.sock)
 # - MC_IMAGE: The name of the MissionControl Docker image to use (default: fmlabs/mc:latest)
 # - MC_CONTAINER_NAME: The name of the MissionControl container to create (default: mc)
-# - MC_PORT: The port to expose for the MissionControl web interface (default: 3443)
+# - MC_PORT: The port to expose for the MissionControl web interface (default: 3080)
+# - MC_PORT_SSL: The port to expose for the MissionControl web interface with SSL (default: 3443)
+# - MC_DATA_DIR: The path to the directory where MissionControl will store its data (default: /opt/mc)
 set -e
 DOCKER=$(which docker)
 DOCKER_HOME=${DOCKER_HOME:-/var/lib/docker}
@@ -16,6 +18,7 @@ DOCKER_SOCKET=${DOCKER_SOCKET:-/var/run/docker.sock}
 MC_IMAGE=${MC_IMAGE:-fmlabs/mc:latest}
 MC_CONTAINER_NAME=${MC_CONTAINER_NAME:-mc}
 MC_PORT=${MC_PORT:-3080}
+MC_PORT_SSL=${MC_PORT_SSL:-3443}
 MC_DATA_DIR=${MC_DATA_DIR:-/opt/mc}
 
 echo "Installing MissionControl with the following configuration:"
@@ -25,6 +28,8 @@ echo "DOCKER_SOCKET=${DOCKER_SOCKET}"
 echo "MC_IMAGE=${MC_IMAGE}"
 echo "MC_CONTAINER_NAME=${MC_CONTAINER_NAME}"
 echo "MC_PORT=${MC_PORT}"
+echo "MC_PORT_SSL=${MC_PORT_SSL}"
+echo "MC_DATA_DIR=${MC_DATA_DIR}"
 
 mkdir -p ${MC_DATA_DIR}
 
@@ -35,6 +40,7 @@ exec $DOCKER run -d \
   --restart always \
   --group-add $(stat -c '%g' ${DOCKER_SOCKET}) \
   -p ${MC_PORT}:3080 \
+  -p ${MC_PORT_SSL}:3443 \
   -v ${DOCKER_SOCKET}:/var/run/docker.sock:ro \
   -v ${MC_DATA_DIR}:/opt/mc \
   ${MC_IMAGE}
